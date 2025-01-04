bd4d205ae856334c94d5aa401a21f7a64cd4dd26
Im-Beast <franik.mateusz@gmail.com>
2025-01-04 22:56:19 +0100

    chore: fmt

--- src/plugins/MusicKitAuthorization/web.ts
+++ src/plugins/MusicKitAuthorization/web.ts
@@ -2,42 +2,39 @@ import { WebPlugin } from "@capacitor/core";
 import type { MusicKitAuthorizationPlugin } from "../MusicKitAuthorization";
 
 // Web implementation of WebKitAuthorization
-export class MusicKitAuthorization extends WebPlugin
-    implements MusicKitAuthorizationPlugin {
-    async authorize(): Promise<
-        { developerToken: string; musicUserToken: string }
-    > {
-        const authorizeMusicKit = async () => {
-            const music = await MusicKit.configure({
-                developerToken: import.meta.env.VITE_DEVELOPER_TOKEN,
-                app: {
-                    name: import.meta.env.VITE_APP_NAME,
-                    build: import.meta.env.VITE_APP_VERSION,
-                },
-            });
+export class MusicKitAuthorization extends WebPlugin implements MusicKitAuthorizationPlugin {
+	async authorize(): Promise<{ developerToken: string; musicUserToken: string }> {
+		const authorizeMusicKit = async () => {
+			const music = await MusicKit.configure({
+				developerToken: import.meta.env.VITE_DEVELOPER_TOKEN,
+				app: {
+					name: import.meta.env.VITE_APP_NAME,
+					build: import.meta.env.VITE_APP_VERSION,
+				},
+			});
 
-            await music.authorize();
-            return music;
-        };
+			await music.authorize();
+			return music;
+		};
 
-        if (globalThis.MusicKit) {
-            const music = await authorizeMusicKit();
+		if (globalThis.MusicKit) {
+			const music = await authorizeMusicKit();
 
-            return {
-                developerToken: music.developerToken,
-                musicUserToken: music.musicUserToken!,
-            };
-        }
+			return {
+				developerToken: music.developerToken,
+				musicUserToken: music.musicUserToken!,
+			};
+		}
 
-        return await new Promise((resolve) => {
-            document.addEventListener("musickitloaded", async () => {
-                const music = await authorizeMusicKit();
+		return await new Promise((resolve) => {
+			document.addEventListener("musickitloaded", async () => {
+				const music = await authorizeMusicKit();
 
-                resolve({
-                    developerToken: music.developerToken,
-                    musicUserToken: music.musicUserToken!,
-                });
-            });
-        });
-    }
+				resolve({
+					developerToken: music.developerToken,
+					musicUserToken: music.musicUserToken!,
+				});
+			});
+		});
+	}
 }
--- src/utils/musickit.ts
+++ src/utils/musickit.ts
@@ -1,18 +1,18 @@
 declare global {
-    // eslint-disable-next-line @typescript-eslint/no-namespace
-    namespace MusicKit {
-        class MusicKitInstance {
-            readonly developerToken: string;
-            musicUserToken?: string;
-            isAuthorized: boolean;
+	// eslint-disable-next-line @typescript-eslint/no-namespace
+	namespace MusicKit {
+		class MusicKitInstance {
+			readonly developerToken: string;
+			musicUserToken?: string;
+			isAuthorized: boolean;
 
-            authorize(): string;
-            unauthorize(): void;
-        }
+			authorize(): string;
+			unauthorize(): void;
+		}
 
-        function getInstance(): MusicKitInstance | undefined;
-        function configure(...args: any): MusicKitInstance;
-    }
+		function getInstance(): MusicKitInstance | undefined;
+		function configure(...args: any): MusicKitInstance;
+	}
 }
 
 export {};
