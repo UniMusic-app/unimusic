8b0aab12e5b384f847102656872f4f3c6be44973
Im-Beast <franik.mateusz@gmail.com>
2025-01-06 12:29:55 +0100

    chore: prepare android env

--- android/app/src/main/java/com/musicplayer/app/MainActivity.java
+++ android/app/src/main/java/com/musicplayer/app/MainActivity.java
@@ -1,4 +1,4 @@
-package io.ionic.starter;
+package com.musicplayer.app;
 
 import com.getcapacitor.BridgeActivity;
 
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-	appId: "com.music-player.app",
+	appId: "com.musicplayer.app",
 	appName: "music-player",
 	webDir: "dist",
 };
