4657538254f22eb1f2f4e96803096656a6151429
Im-Beast <franik.mateusz@gmail.com>
2025-01-06 15:43:09 +0100

    feat: MusicKitAuthorizationPlugin for Android
    MusicKit Web `window.open` method doesn't work on Android either.
    So it uses a native android plugin using MusicKit for Android library [`com.apple.android.sdk.authentication`](https://developer.apple.com/musickit/android/).
    Tested on an Android 11 device as well as Android Studio's emulator running Android 15.

--- android/app/src/main/java/com/musicplayer/app/MainActivity.java
+++ android/app/src/main/java/com/musicplayer/app/MainActivity.java
@@ -1,5 +1,13 @@
 package com.musicplayer.app;
 
+import android.os.Bundle;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        registerPlugin(MusicKitAuthorizationPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
