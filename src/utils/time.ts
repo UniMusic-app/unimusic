6f10274118b3922867462e8ff02b70a8438d2698
Im-Beast <franik.mateusz@gmail.com>
2025-02-10 15:07:52 +0100

    fix: invalid second to MM:SS conversion

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,5 +1,5 @@
 export function secondsToMMSS(time: number): string {
-	const minutes = String(Math.round(time / 60)).padStart(2, "0");
-	const seconds = String(Math.round(time % 60)).padStart(2, "0");
+	const minutes = String(Math.floor(time / 60)).padStart(2, "0");
+	const seconds = String(Math.floor(time % 60)).padStart(2, "0");
 	return `${minutes}:${seconds}`;
 }
