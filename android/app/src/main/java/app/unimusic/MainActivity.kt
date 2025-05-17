bf47b9d3e98a7de0e865a70edf9b2dc8186776a4
Im-Beast <franik.mateusz@gmail.com>
2025-05-17 14:17:13 +0200

    feat(android): add unimusic-sync dependency

--- android/app/src/main/java/app/unimusic/MainActivity.kt
+++ android/app/src/main/java/app/unimusic/MainActivity.kt
@@ -1,16 +1,44 @@
 package app.unimusic
 
 import android.os.Bundle
+import app.unimusic.sync.UniMusicSync
 import com.getcapacitor.BridgeActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
 
 class MainActivity : BridgeActivity() {
+    private val appScope = CoroutineScope(SupervisorJob())
+    lateinit var uniMusicSync: UniMusicSync
+        private set
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        appScope.launch {
+            val sync = UniMusicSync.create(applicationContext.cacheDir.path);
+            val author = sync.irohManager.getAuthor();
+            uniMusicSync = sync
+            println("IROH Author: $author")
+        }
+
         registerPlugin(MusicKitAuthorizationPlugin::class.java)
         registerPlugin(LocalMusicPlugin::class.java)
+
         super.onCreate(savedInstanceState)
     }
 
+    override fun onDestroy() {
+        if (::uniMusicSync.isInitialized) {
+            uniMusicSync.irohManager.destroy()
+        }
+        super.onDestroy()
+    }
+
     override fun onStart() {
+        if (::uniMusicSync.isInitialized) appScope.launch {
+            val author = uniMusicSync?.irohManager?.getAuthor();
+            println("IROH Author: $author")
+        }
+
         val bridge = super.getBridge()
         val webView = bridge.webView
         webView.webViewClient = CleanWebViewClient(bridge)
