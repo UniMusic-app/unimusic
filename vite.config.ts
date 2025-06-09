de62da690b7f363edda3bdbb1a3197940216a691
Im-Beast <franik.mateusz@gmail.com>
2025-06-09 12:33:44 +0200

    fix: add polyfills for newer features to support older targets

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
+import legacy from "@vitejs/plugin-legacy";
 import vue from "@vitejs/plugin-vue";
+
 import { defineConfig } from "vite";
 import capacitorConfig from "./capacitor.config";
 import { sharedAlias, sharedBuild, sharedDefine } from "./shared.vite.config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [vue()],
+	plugins: [vue(), legacy({ modernTargets: "defaults", modernPolyfills: true })],
 	build: {
 		outDir: capacitorConfig.webDir,
+		cssTarget: "chrome109",
 		...sharedBuild,
 	},
 	define: {
