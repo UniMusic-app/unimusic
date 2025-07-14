42ac658b5297aad51f3886e6e270e941f1aaa998
Im-Beast <franik.mateusz@gmail.com>
2025-07-14 12:18:10 +0200

    fix(iOS): crashing on iOS 26

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -1,6 +1,13 @@
 import Capacitor
+import Network
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        // NOTE: Without this the application crashes on iOS 26
+        nw_tls_create_options()
+        return true
+    }
 }
